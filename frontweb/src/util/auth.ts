46292cfe2f40722b1c6ca6a1d7298a9f21e52b38
mateus-hub <theusskt2018@gmail.com>
2021-10-19 14:04:12 -0300

    frontweb

---
+++ frontweb/src/util/auth.ts
@@ -0,0 +1,32 @@
+import { Role } from "types/role";
+import { getTokenData } from "./token";
+
+export const isAuthenticated = (): boolean => {
+  const tokenData = getTokenData();
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
+
+//função p testar dado uma lista de 'roles' perfis de usuário, se o usuario atual possui algum role da lista
+export const hasAnyRoles = (roles: Role[]): boolean => {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  // pegar dados do usuario logado no localstorage
+  const tokenData = getTokenData();
+
+  /* Forma alternativa "Função de alta ordem"
+    if(tokenData !== undefined) {
+      return roles.some(role => tokenData.authorities.includes(role));
+    }*/
+
+  if (tokenData !== undefined) {
+    for (var i = 0; i < roles.length; i++) {
+      if (tokenData.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
